d73f053c684b56b8051c40607063f7d97bcf4975
Kian <108917081+kontobbbsbaan@users.noreply.github.com>
2022-09-10 22:55:08 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,15 +2,15 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*`
+let str = `\n`
 let wibu = `https://telegra.ph/file/385cfe85ba26deda1726d.jpg` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/_ctzhid",
     mediaType: "IMAGE",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'MODZ BOTZ MultiDevice',
-    body: wm,
+    description: "https://telegra.ph/file/385cfe85ba26deda1726d.jpg", 
+    title: 'MODZ BOTZ MULTI DEVICE',
+    body: 'MAU SCRIPT NYA? CHAT OWNER BOT',
     thumbnail: thumb,
     sourceUrl: sgc
   }
