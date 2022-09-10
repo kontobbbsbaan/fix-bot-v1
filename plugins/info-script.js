1b4cd7dd73c68c5f056f118095cbdee2187f028c
Kian <108917081+kontobbbsbaan@users.noreply.github.com>
2022-09-11 06:06:08 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -5,8 +5,8 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let str = `\n`
 let wibu = `https://telegra.ph/file/385cfe85ba26deda1726d.jpg` 
 let thumb = await(await fetch(wibu)).buffer()
-conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+conn.sendButtonDoc(m.chat, str, 'MAU SCRIPT NYA? CHAT OWNER BOT','Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://chat.whatsapp.com/FvDujLzzrQsGaJaRLVZ0Fg",
     mediaType: "IMAGE",
     description: "https://telegra.ph/file/385cfe85ba26deda1726d.jpg", 
     title: 'MODZ BOTZ MULTI DEVICE',
